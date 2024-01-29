e24324ebf6289dac28cc0fca7550e79e456c44d6
Tiramister <nh111424@gmail.com>
2024-01-29 12:55:41 +0900

    feat(SPA): Hello, world.

---
+++ induction-training-2023/spa/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Hello from "./components/Hello";
+
+export default function App() {
+  return <Hello name="world" />;
+  // Hello({ name: "world" }) と等価
+}
---
+++ induction-training-2023/spa/src/components/Hello.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Hello(props: { name: string }) {
+  return <div>Hello {props.name}!</div>;
+}
+
