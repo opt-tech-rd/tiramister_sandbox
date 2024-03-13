5ef824080044254db88f2a02957d64aec6f79590
Tiramister <nh111424@gmail.com>
2024-03-13 11:12:19 +0900

    feat(kotlin-sql-demo): 試しに実装

--- kotlin-sql-demo/build.gradle.kts
+++ kotlin-sql-demo/build.gradle.kts
@@ -9,7 +9,15 @@ repositories {
     mavenCentral()
 }
 
+val exposedVersion: String by project
 dependencies {
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+
+    implementation("org.postgresql:postgresql:42.7.1")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
--- kotlin-sql-demo/src/main/kotlin/Main.kt
+++ kotlin-sql-demo/src/main/kotlin/Main.kt
@@ -1,5 +1,46 @@
 package org.example
 
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object TestTable: IntIdTable("test_exposed") {
+    val name = varchar("name", 50)
+    val age = integer("age")
+}
+
 fun main() {
-    println("Hello World!")
+    val host = "### SENSORED ###"
+    val port = 5432
+    val database = "### SENSORED ###"
+    val username = "### SENSORED ###"
+    val password = "### SENSORED ###"
+
+    Database.connect(
+        "jdbc:postgresql://$host:$port/$database",
+        driver = "org.postgresql.Driver",
+        user = username,
+        password = password
+    )
+
+    transaction {
+        addLogger(StdOutSqlLogger)
+
+        // テーブル作成
+        SchemaUtils.create(TestTable)
+
+        TestTable.insert {
+            it[name] = "Hanako"
+            it[age] = 30
+        }
+
+        TestTable.selectAll().forEach { row ->
+            println("${row[TestTable.id]}: ${row[TestTable.name]} is ${row[TestTable.age]} years old")
+        }
+    }
 }
