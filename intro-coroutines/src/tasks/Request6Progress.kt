fbc8b0b11a0b7e336371502c76bbd7fd30549dd1
Tiramister <nh111424@gmail.com>
2024-03-19 14:25:38 +0900

    feat(intro-coroutines): Task 6

--- intro-coroutines/src/tasks/Request6Progress.kt
+++ intro-coroutines/src/tasks/Request6Progress.kt
@@ -7,5 +7,18 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service.getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val allUsers = repos.foldIndexed(emptyList<User>()) { index, accUsers, repo ->
+        val users = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        val nextAccUsers = (accUsers + users).aggregate()
+        updateResults(nextAccUsers, index == repos.lastIndex)
+        nextAccUsers
+    }
 }
