079d8c60102bdf16df669b88e406c49994e56e9b
Tiramister <nh111424@gmail.com>
2024-03-21 15:23:51 +0900

    feat(intro-coroutines): Task 7

--- intro-coroutines/src/tasks/Request7Channels.kt
+++ intro-coroutines/src/tasks/Request7Channels.kt
@@ -1,6 +1,7 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -11,6 +12,33 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service.getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+
+        val usersChannel = Channel<List<User>>()
+
+        // consumer: チャンネルからユーザーを受け取り、画面を更新する
+        launch {
+            var allUsers = emptyList<User>()
+            repeat(repos.size) {
+                val users = usersChannel.receive()
+                allUsers = (allUsers + users).aggregate()
+                updateResults(allUsers, it == repos.lastIndex)
+            }
+        }
+
+        // receiver: ユーザーを取得し、チャンネルに送る
+        for (repo in repos) {
+            launch {
+                val users = service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                usersChannel.send(users)
+            }
+        }
+
+        // Structured concurrency により、全てのコルーチンが終了するまで待つ
     }
 }
